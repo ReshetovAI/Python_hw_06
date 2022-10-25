3eb0dd0882382bd11d5be92a1c76b5fc6ebf7ee5
Aleksandr <alexresh1@rambler.ru>
2022-10-25 21:32:20 +0300

    task_03

---
+++ task_03_hw3_2.py
@@ -0,0 +1,31 @@
+# Задача 3
+
+# Напишите программу, которая найдёт произведение пар чисел списка.
+# Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+
+# Пример:
+# [2, 3, 4, 5, 6] => [12, 15, 16]
+# [2, 3, 5, 6] => [12, 15]
+
+# from random import randint
+# import math
+
+# size = int(input('Введите размер списка: '))
+# my_list = []
+# for i in range(size):
+#     my_list.append(randint(-10, 10))
+
+# print('Список: ', my_list)
+
+
+# def mult_pairs(my_list):
+#     return [my_list[i] * my_list[-i - 1] for i in range(math.ceil(len(my_list)/2))]
+
+
+# print('Произведение пар списка: ', mult_pairs(my_list))
+
+import math
+my_list = list(map(int, input('Введите числа, через пробел: ').split()))
+print('Исходный список: ', my_list)
+print('Произведение пар списка: ', list(
+    [a*b for a, b in zip(my_list[0:math.ceil(len(my_list)/2)], my_list[::-1])]))
